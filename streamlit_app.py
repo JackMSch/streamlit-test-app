abfa7206f8d20735f3b7738a3c1d3c48a170bb29
JackMSch <146956988+JackMSch@users.noreply.github.com>
2026-06-16 15:35:06 -0400

    v0.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import datetime
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.write("Shawty like a melody some lyrics i dont remember")
+
+st.autorefresh(interval=1000)
+st.write(datetime.time)
